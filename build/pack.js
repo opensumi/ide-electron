c88a986d339b87799c284d9f3ff02b4263df089d
yutian.yz <yutian.yz@alibaba-inc.com>
2021-12-20 20:59:00 +0800

    chore: specifies the release version

--- build/pack.js
+++ build/pack.js
@@ -3,11 +3,28 @@ const path = require('path');
 const electronBuilder = require('electron-builder');
 const rootPackage = require('../package.json');
 
+const DEFAULT_TARGET_PLATFORM = 'darwin';
+
 // 使用双 package.json 结构，自动处理 node_modules
 fs.copyFileSync(path.join(__dirname, '../build/package.json'), path.join(__dirname, '../app/package.json'));
 
+const targetPlatforms = (process.env.TARGET_PLATFORMS || DEFAULT_TARGET_PLATFORM).split(',').map((str) => str.trim());
+const targets = new Map();
+if (targetPlatforms.includes('win32')) {
+  targets.set(electronBuilder.Platform.WINDOWS, new Map([
+    [electronBuilder.Arch.x64, ['nsis']],
+  ]));
+}
+if (targetPlatforms.includes('darwin')) {
+  targets.set(electronBuilder.Platform.MAC, new Map([
+    [electronBuilder.Arch.x64, ['dmg']],
+    // [electronBuilder.Arch.arm64, ['dmg']],
+  ]));
+}
+
 electronBuilder.build({
   publish: null,
+  targets: targets.size ? targets : undefined,
   config: {
     productName: 'OpenSumi-Desktop',
     npmArgs: ['--registry=https://registry.npm.taobao.org'],
