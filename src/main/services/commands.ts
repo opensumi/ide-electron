3e82c462558d15e877834d456203a10f3fe87a9b
野声 <lijiacheng.ljc@antgroup.com>
2022-03-15 13:18:30 +0800

    feat: add cmd tools

---
+++ src/main/services/commands.ts
@@ -0,0 +1,56 @@
+import { Autowired, Injectable, Injector, INJECTOR_TOKEN } from "@opensumi/di";
+import { electronEnv } from "@opensumi/ide-core-browser";
+import {
+  Domain,
+  CommandContribution,
+  CommandRegistry,
+} from "@opensumi/ide-core-common";
+import { IMainLayoutService } from "@opensumi/ide-main-layout";
+import { ITerminalController } from "@opensumi/ide-terminal-next";
+
+@Domain(CommandContribution)
+export class MainCommandContribution implements CommandContribution {
+  @Autowired(INJECTOR_TOKEN)
+  injector: Injector;
+
+  @Autowired(ITerminalController)
+  terminalService: ITerminalController;
+
+  @Autowired(IMainLayoutService)
+  private mainLayoutService: IMainLayoutService;
+
+  registerCommands(registry: CommandRegistry): void {
+    registry.registerCommand(
+      {
+        id: "sumi.commandLineTool.install",
+        label: "install command line sumi",
+      },
+      {
+        execute: async () => {
+          if (this.terminalService.clients.size === 0) {
+            await this.terminalService.createClientWithWidget2({
+              terminalOptions: {
+                name: "install sumi",
+                cwd: "/usr/local/bin",
+              },
+            });
+          }
+
+          const handler = this.mainLayoutService.getTabbarHandler("terminal");
+          if (handler) {
+            handler.activate();
+          }
+          const binPath = `${electronEnv.env.MAC_CONTENTS_PATH}/Resources/resources/darwin/bin/sumi`;
+          const command = `cd /usr/local/bin && ln -sf '${binPath}' && chmod +x ./sumi\n`;
+
+          for (const c of this.terminalService.clients.values()) {
+            if (c) {
+              c.sendText(command);
+              return;
+            }
+          }
+        },
+      }
+    );
+  }
+}
--- src/main/services/index.ts
+++ src/main/services/index.ts
@@ -1,18 +1,17 @@
-import { Autowired, Injectable, Injector, INJECTOR_TOKEN } from '@opensumi/di';
-import { ElectronMainModule } from '@opensumi/ide-core-electron-main/lib/electron-main-module';
-import { IHelloService } from '../../common/types';
-import { HelloContribution, HelloService } from './hello';
+import { Autowired, Injectable, Injector, INJECTOR_TOKEN } from "@opensumi/di";
+import { ElectronMainModule } from "@opensumi/ide-core-electron-main/lib/electron-main-module";
+import { IHelloService } from "../../common/types";
+import { MainCommandContribution } from "./commands";
+import { HelloContribution, HelloService } from "./hello";
 
 @Injectable()
 export class MainModule extends ElectronMainModule {
-
   providers = [
     {
       token: IHelloService,
       useClass: HelloService,
     },
     HelloContribution,
-  ]
-
+    MainCommandContribution,
+  ];
 }
-
