ee4a540219c4b4e8c6b397bcb5e066628222259c
野声 <lijiacheng.ljc@antgroup.com>
2022-04-12 19:05:24 +0800

    fix: update import path

--- src/node/index.ts
+++ src/node/index.ts
@@ -4,10 +4,10 @@ import { NodeModule, ConstructorOf } from '@opensumi/ide-core-node';
 import { ServerCommonModule } from '@opensumi/ide-core-node';
 import { FileServiceModule } from '@opensumi/ide-file-service/lib/node';
 
-import { ProcessModule } from '@opensumi/ide-process';
+import { ProcessModule } from '@opensumi/ide-process/lib/node';
 
-import { FileSearchModule } from '@opensumi/ide-file-search';
-import { SearchModule } from '@opensumi/ide-search';
+import { FileSearchModule } from '@opensumi/ide-file-search/lib/node';
+import { SearchModule } from '@opensumi/ide-search/lib/node';
 import { TerminalNodePtyModule } from '@opensumi/ide-terminal-next/lib/node';
 import { LogServiceModule } from '@opensumi/ide-logs/lib/node';
 import { ExtensionModule } from '@opensumi/ide-extension/lib/node';
